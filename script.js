633777dce6df20df4a5c1278126096a192f38d8c
Sven-27 <github.geometric664@passinbox.com>
2025-06-25 20:54:42 +0200

    email validation works

--- script.js
+++ script.js
@@ -2,19 +2,23 @@ const errorText = document.querySelector('.error-text');
 const emailInput = document.querySelector('input[type="text"]');
 
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-console.log(emailInput.value);
 function validateEmail() {
   const email = emailInput.value;
-  console.log(email);
   if (email === '' || !emailRegex.test(email)) {
     errorText.classList.remove('d-none')
     emailInput.classList.add('error')
     return false;
   } 
+  else {
+    errorText.classList.add('d-none');
+    emailInput.classList.remove('error');
+    emailInput.value = ''; // Clear the input field
+    // Here you can add the code to handle the form submission, e.g., sending data to a server
+    console.log("Form submitted successfully!");
+    }
 }
 
 function handleSubmit(event) {
-    event.preventDefault();
-    console.log(validateEmail());
-  console.log(emailInput.value);
-}
+  event.preventDefault();
+  validateEmail()  
+} 
